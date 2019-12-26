6bd799391c312042edce4104295842fce0c31dbd
Ruslan Molchanov <ruslan.molchanov@specific-group.com>
2019-12-26 10:40:58 +0300

    Init commit

---
+++ src/main/kotlin/me/ruslanys/ifunny/crawler/Application.kt
@@ -0,0 +1,11 @@
+package me.ruslanys.ifunny.crawler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/me/ruslanys/ifunny/crawler/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.ruslanys.ifunny.crawler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
