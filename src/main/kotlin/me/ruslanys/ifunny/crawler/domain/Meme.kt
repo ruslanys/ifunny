85011284aaa8df0aa75f155cb4b52a949e83a54a
Ruslan Molchanov <ruslanys@gmail.com>
2019-12-27 17:34:00 +0300

    Domain Document

---
+++ src/main/kotlin/me/ruslanys/ifunny/crawler/domain/Meme.kt
@@ -0,0 +1,30 @@
+package me.ruslanys.ifunny.crawler.domain
+
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.MongoId
+import java.time.LocalDateTime
+
+@Document
+data class Meme(
+
+        @Indexed
+        val language: String,
+        val sourceName: String,
+
+        @Indexed(unique = true)
+        val pageUrl: String,
+        val title: String,
+
+        var resourceUrl: String,
+
+        val publishDateTime: LocalDateTime? = null,
+        val author: String? = null,
+        val likes: Int? = null,
+        val comments: Int? = null,
+
+        @MongoId
+        var id: ObjectId = ObjectId()
+
+)
