2d8b58d48799f77b3a8189ba7af474261e98b08b
Ruslan Molchanov <ruslanys@gmail.com>
2020-01-10 14:50:17 +0300

    AWS S3 Properties validation

--- src/main/kotlin/me/ruslanys/ifunny/property/AwsS3Properties.kt
+++ src/main/kotlin/me/ruslanys/ifunny/property/AwsS3Properties.kt
@@ -3,21 +3,21 @@ package me.ruslanys.ifunny.property
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.validation.annotation.Validated
-import javax.validation.constraints.NotNull
+import javax.validation.constraints.NotEmpty
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "aws.s3")
 @Validated
 data class AwsS3Properties(
-        @field:NotNull
+        @field:NotEmpty
         val accessKey: String? = null,
 
-        @field:NotNull
+        @field:NotEmpty
         val secretKey: String? = null,
 
-        @field:NotNull
+        @field:NotEmpty
         val region: String? = null,
 
-        @field:NotNull
+        @field:NotEmpty
         val bucket: String? = null
 )
