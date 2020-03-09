baf3948d7092a3cc2fc39b411dfaec2ab9456be1
Ruslan Molchanov <ruslanys@gmail.com>
2020-03-09 22:35:57 +0300

    An interesting observation

--- src/test/kotlin/me/ruslanys/ifunny/ApplicationTests.kt
+++ src/test/kotlin/me/ruslanys/ifunny/ApplicationTests.kt
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+/**
+ * https://github.com/spring-projects/spring-boot/issues/19994
+ * https://youtrack.jetbrains.com/issue/KT-34024
+ */
 @Disabled
 @SpringBootTest
 class ApplicationTests {
